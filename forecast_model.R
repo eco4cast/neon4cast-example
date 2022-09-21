15d5ec1b7a8b0fb50007f3acfa7490a2b4aa447e
rqthomas <rqthomas@vt.edu>
2022-09-21 13:35:27 -0400

    is it the second call to collect?

--- forecast_model.R
+++ forecast_model.R
@@ -44,14 +44,14 @@ sites <- sites[1]
     dplyr::select(time, predicted, ensemble) |>
     dplyr::collect()
 
-  #noaa_future <- df_future |>
-  #  dplyr::filter(cycle == 0,
-  #                site_id == sites[i],
-  #                start_date == as.character(noaa_date),
-  #                time >= lubridate::as_datetime(forecast_date),
-  #                variable == "air_temperature") |>
-  #  dplyr::select(time, predicted, ensemble) |>
-  #  dplyr::collect()
+  noaa_future <- df_future |>
+    dplyr::filter(cycle == 0,
+                  site_id == sites[i],
+                  start_date == as.character(noaa_date),
+                  time >= lubridate::as_datetime(forecast_date),
+                  variable == "air_temperature") |>
+    dplyr::select(time, predicted, ensemble) |>
+    dplyr::collect()
 #   
 #   # Aggregate (to day) and convert units of drivers
 #   
